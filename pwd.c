b6ac250b4ed3c13ccbd29954d51178162412fc1e
Hps2702 <102587795+Hps2702@users.noreply.github.com>
2023-08-10 12:00:29 +0530

    Delete pwd.c

--- pwd.c
+++
@@ -1,9 +0,0 @@
-#include "headers.h"
-void execute_pwd(char **command)
-{
-    char *cur_wd = malloc(N);
-    getcwd(cur_wd, N);
-    strcpy(dir_store, cur_wd);
-    printf("%s\n", cur_wd);
-   // fprintf(stderr,"%s\n",cur_wd);
-}
